ea174048f02a998ece5df6d2465020e6d2029759
Digrol <Digrol@gmail.com>
2020-02-09 22:52:20 +0300

    d5 ::  developing

--- trunk/app/SelfTest/src/SelfTest.java
+++ trunk/app/SelfTest/src/SelfTest.java
@@ -1,11 +1,12 @@
 
+import java.util.*;
+
 import org.denom.*;
 import org.denom.log.*;
-import org.denom.D5.D5Command;
+import org.denom.d5.*;
 
-import static org.denom.Binary.Bin;
-import static org.denom.Ex.MUST;
-
+//import static org.denom.Binary.Bin;
+//import static org.denom.Ex.MUST;
 
 // -----------------------------------------------------------------------------------------------------------------
 public class SelfTest
@@ -14,24 +15,21 @@ public class SelfTest
 
 	public static void main( String[] args )
 	{
-		Binary b = Bin().reserve( 11000000 );
+		D5Client client = new D5Client( "denom.org", 4210 );
 
-		Ticker t = new Ticker();
+		Ticker ticker = new Ticker();
 		
-		Binary data = new Binary(10000000, 0x05);
-		D5Command command = new D5Command();
-		command.index = 2;
-		command.code = D5Command.ENUM_COMMANDS;
-		command.data = data;
-		
-		command.encode( b );
-		
-		D5Command command2 = new D5Command();
-		command2.decode( b );
-		
-		MUST( command2.equals( command ) );
-		log.writeln( "" + t.getDiffMs() );
+		for( int i = 0; i < 10; ++i )
+		{
+			Collection<Integer> cmds = client.commandEnumCommands();
+		}
 
-		log.writeln( String.format( " \n%08X", command2.index ) );
+		log.writeln( "" + ticker.getDiffMs() );
+
+		Collection<Integer> cmds = client.commandEnumCommands();
+		for( int cmd : cmds )
+			log.writeln( String.format( "%8X", cmd ) );
+
+		
 	}
 }
